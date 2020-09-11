29a10a00a9ae4b3eaa6ba38ad123fbeeaba69fa1
Yehor Pliasov <fenix4088@gmail.com>
2020-09-11 13:04:11 +0200

    SortableTable v1, Notification изменил имя статического свойства, Column-Chart изменил let на const !!

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,88 @@
 export default class ColumnChart {
-
-}
+    chartHeight = 50;
+    markupWrapper = {}
+  
+    constructor(obj = {}) {
+      this.data = obj.data || [];
+      this.label = obj.label || "";
+      this.value = obj.value;
+      this.link = obj.link;
+  
+      this.render();
+      this.renderLink();
+    }
+  
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = this.markup;
+  
+      this.element = element.firstElementChild;
+      if (this.data.length) {
+        this.element.classList.remove("column-chart_loading");
+      }
+  
+      this.markupWrapper = this.getMarkupWrapper(this.element);
+    }
+  
+    get markup() {
+      return `   
+      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
+      <div class="column-chart__title">
+        Total ${this.label}
+      </div>
+      <div class="column-chart__container">
+      <div class="column-chart__header">${this.value}</div>
+      <div data-element="body" class="column-chart__chart">
+        ${this.createChartElements(this.data)}
+      </div>
+      </div>
+    </div>`;
+    }
+  
+    renderLink() {
+      const linkMarkup = `<a href="/sales" class="column-chart__link">View all</a>`;
+      if (this.link) {
+        const title = this.element.querySelector(".column-chart__title");
+        title.innerHTML = `Total ${this.label} ${linkMarkup}`;
+      }
+    }
+  
+    createChartElements(data) {
+      const maxNumber = Math.max(...data);
+  
+      const newElement = data.map((dataItem) => {
+  
+          let value = Math.floor((dataItem * this.chartHeight) / maxNumber);
+          let percent = ((dataItem * 100) / maxNumber).toFixed(0);
+  
+          return `<div style="--value:${value}" data-tooltip="${percent}%"></div>`;
+  
+        }).join("");
+  
+        return newElement;
+    }
+  
+    getMarkupWrapper(element) {
+      const markupWrappers = element.querySelectorAll('[data-element]');
+      return [...markupWrappers].reduce((acc, markupWrapper) => {
+        acc[markupWrapper.dataset.element] = markupWrapper;
+  
+        return acc;
+      }, {});
+    }
+  
+    update(data = []) {
+      this.data = data;
+      this.markupWrapper.body.innerHTML = this.createChartElements(data)
+    }
+  
+    remove() {
+      this.element.remove();
+    }
+  
+    destroy() {
+      this.remove();
+      this.element = null;
+    }
+  }
+  
--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,54 @@
 export default class NotificationMessage {
-
-}
+    element;
+    static notificationStatus;
+  
+    constructor(messageText, { duration = 2000, type = 'success' } = {}) {
+      if(NotificationMessage.notificationStatus) {
+          NotificationMessage.notificationStatus.remove(); 
+      }
+      this.messageText = messageText;
+      this.duration = duration;
+      this.type = type;
+      this.render();
+    }
+  
+    get markup() {
+      return `
+          <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+          <div class="timer"></div>
+          <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+              ${this.messageText}
+          </div>
+          </div>
+      </div>
+        `;
+    }
+  
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = this.markup;
+      this.element = element.firstElementChild;
+  
+      NotificationMessage.notificationStatus = this.element;
+  
+    }
+  
+    show(target = document.body) {
+      target.append(this.element);
+      setTimeout(() => {
+        this.remove();
+        this.destroy();
+      }, this.duration);
+    }
+  
+    remove() {
+      this.element.remove();
+    }
+  
+    destroy() {
+      this.element = null;
+    }
+  }
+  
